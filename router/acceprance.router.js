8a5e95dbbea42699b80a1c72437e3f1c0e9ddffc
Shukurulla <shukurullatursinbayev978@gmail.com>
2024-05-27 17:24:41 +0500

    added acceprance route

--- router/acceprance.router.js
+++ router/acceprance.router.js
@@ -6,9 +6,9 @@ const router = Router();
 router.post("/post-acceptance", async (req, res) => {
   const acceptance = await Acceptance.create(req.body);
   if (!acceptance) {
-    req.status(501).json({ status: "Failure", mag: "Error" });
+    res.status(501).json({ status: "Failure", mag: "Error" });
   } else {
-    req
+    res
       .status(200)
       .json({ data: acceptance, status: "Success", msg: "Successful" });
   }
